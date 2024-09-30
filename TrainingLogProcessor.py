46ec0409f1bc1203037bf01d2d456877b2b6bb53
YudaiTaka0515 <YudaiTakahashi0515@gmail.com>
2024-09-30 18:26:58 +0900

    first commit

---
+++ TrainingLogProcessor.py
@@ -0,0 +1,112 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+import pandas as pd
+import datetime as dt
+
+
+class TrainingLogProcessor:
+    def __init__(self):
+        # self.df = self.__load_from_gss()  
+        self.df = self.__read_spreadsheet(sheet_name="TrainingLog",
+                                          crendential_path='credentials.json')
+
+    def __load_from_gss(sheet_name="TrainingLog", credential_path="credentials.json"):
+        scope = ["https://spreadsheets.google.com/feeds", 
+                 "https://www.googleapis.com/auth/drive"]
+        # 認証情報を使ってクライアントを作成
+        creds = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scope)
+        client = gspread.authorize(creds)
+
+        # スプレッドシートを開く (スプレッドシートの名前を指定)
+        print("a")
+        spreadsheet = client.open(sheet_name)
+        print("a")
+
+        # シートを選択
+        sheet = spreadsheet.sheet1  # 一番目のシートを選択する場合
+
+        # データを読み込む
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
+        df["日付"] = pd.to_datetime(df["日付"])
+
+        # training_menus = ["バックスクワット", "ベンチプレス", "ダンベルベンチプレス", "コンベンショナルデットリフト"]
+        # df = df[df["種目"].isin(training_menus)]
+        df["Volume"] = df["重量[kg]"] * df["回数[回]"]
+
+        return df
+
+    def __read_spreadsheet(self, sheet_name:str, 
+                           crendential_path:str):
+        # スコープの設定
+        scope = ["https://spreadsheets.google.com/feeds", 
+                 "https://www.googleapis.com/auth/drive"]
+        # 認証情報を使ってクライアントを作成
+        creds = ServiceAccountCredentials.from_json_keyfile_name(crendential_path, scope)
+        client = gspread.authorize(creds)
+
+        # スプレッドシートを開く (スプレッドシートの名前を指定)
+        spreadsheet = client.open(sheet_name)
+
+        # シートを選択
+        sheet = spreadsheet.sheet1  # 一番目のシートを選択する場合
+
+        # データを読み込む
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
+        df["日付"] = pd.to_datetime(df["日付"])
+        df["Volume"] = df["重量[kg]"] * df["回数[回]"]
+
+        return df
+
+    def get_latest_volume(self):
+        # 各種目の最終日の記録のみ表示する
+        # 前回の重量を取得
+        total_volume_df = self.df.groupby(["日付", "種目"])["Volume"].sum().reset_index()
+        total_volume_df = total_volume_df.rename(columns={"Volume" : "Total Volume"})
+        total_volume_df['Pre Volume'] = total_volume_df.groupby('種目')['Total Volume'].shift(1)
+
+        # 各種目の最終日のデータを抽出
+        latest_df = total_volume_df.groupby('種目').tail(1)
+        latest_df["Delta"] = latest_df["Total Volume"] - latest_df["Pre Volume"]
+
+        latest_df = latest_df.fillna(0)
+        latest_df
+
+        return latest_df
+
+    def get_total_volume(self):
+        total_volume_df = self.df.groupby(["日付", "種目"])["Volume"].sum().reset_index()
+        total_volume_df = total_volume_df.pivot(index="日付",
+                                                columns="種目",
+                                                values="Volume").reset_index()
+        return total_volume_df
+
+    def get_contribution_map(self):
+        # 日付のためのdfを作成する
+        year = dt.datetime.today().strftime("%Y")
+        date_df = pd.DataFrame()
+
+        date_df["date"] = pd.date_range(f'{year}-01-01', f'{year}-12-31')
+        date_df["date"] = date_df["date"].dt.floor('D')
+
+        daily_volume_df = self.df.groupby("日付")["Volume"].sum().reset_index()
+        daily_volume_df = daily_volume_df.rename(
+            columns={"Volume" : "Total Volume"}
+        )
+        contribution_df = pd.merge(date_df, daily_volume_df, left_on="date",
+                                   right_on="日付", how="left")
+        contribution_df = contribution_df.fillna(0)
+        contribution_df.set_index('date', inplace=True)
+        # add data
+        contribution_df['weekday'] = pd.to_datetime(contribution_df.index).strftime('%w')
+        contribution_df['week'] = pd.to_datetime(contribution_df.index).strftime('%U')
+
+        contribution_map = contribution_df.pivot(index='weekday',
+                                                 columns='week',
+                                                 values='Total Volume').reset_index()
+        contribution_map.drop(['weekday'], axis=1, inplace=True)
+        contribution_map.fillna(0, inplace=True)
+
+        return contribution_map
---
+++ Visuzlizer.py
@@ -0,0 +1,114 @@
+from TrainingLogProcessor import TrainingLogProcessor
+import streamlit as st
+# import plotly.express as px
+import plotly.graph_objects as go
+import datetime as dt
+import pandas as pd
+
+
+
+class TrainingLogVisualizer:
+    def __init__(self):
+        self.logs = TrainingLogProcessor()
+        self.target_menu = ['バックスクワット',
+                            'ベンチプレス',
+                            'コンベンショナルデッドリフト',
+                            'ダンベルベンチプレス'
+                            ]
+
+        self.total_volume_df = self.logs.get_total_volume()
+        self.rows, self.cols = 2, 2
+
+    def visualize_volume(self):
+        latest_df = self.logs.get_latest_volume()
+        for r in range(0, self.rows):
+            columns = st.columns(self.cols)
+            for c in range(0, self.cols):
+                idx = c + r * self.cols
+                if idx >= len(self.target_menu):
+                    break
+                menu_name = self.target_menu[idx]
+                target_rec = latest_df[latest_df["種目"] == menu_name].iloc[0]
+                with columns[c]:
+                    # カードビューの描画
+                    st.metric(label=target_rec["種目"],
+                              value=f'{target_rec["Total Volume"]}kg',
+                              delta=f'{target_rec["Delta"]}kg')
+                    # TotalVolumeの推移を出力
+                    self.__plot_target_volume(menu_name=menu_name)
+
+    def __plot_target_volume(self, menu_name):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=self.total_volume_df["日付"],
+                                 y=self.total_volume_df[menu_name],
+                                 connectgaps=True))
+
+        fig.update_xaxes(rangeslider={"visible" : True})  # X軸に range slider を表示（下図参照）
+        fig.update_layout(title=menu_name)  # グラフタイトルを設定
+        fig.update_layout(font={"family" : "Meiryo", "size" : 15})
+        # fig.u
+
+        displayed_day = 14
+        print(dt.datetime.today() - dt.timedelta(displayed_day))
+        fig.update_layout(template="seaborn",
+                          xaxis_title="Date",
+                          yaxis_title="Total Volume[kg]",
+                          width=800,
+                          height=600,
+                          xaxis=dict(
+                              range=[dt.datetime.today() -
+                                     dt.timedelta(days=displayed_day),
+                                     dt.datetime.today()]))
+        st.plotly_chart(fig, use_container_width=True)
+
+    def plot_contribution_map(self):
+        contribution_map = self.logs.get_contribution_map()
+
+        fig = go.Figure(data=go.Heatmap(
+                z=contribution_map.values,  # ヒートマップの値
+                xgap=4,  # X方向のグリッド間のギャップ
+                ygap=4,  # Y方向のグリッド間のギャップ
+                colorscale='Greens', # カラースケール
+                showscale=False  # カラーバーを非表示（GitHub風に）
+            ))
+
+        # x軸ラベルとして使用する月ごとのマーカー位置（週番号）
+        year = dt.datetime.today().strftime("%Y")
+        dates = pd.date_range(f'{year}-01-01', f'{year}-12-31')
+        weeks = dates.strftime("%U").astype(int) + 1
+        month_start_weeks = weeks[dates.is_month_start]
+        month_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+        # x軸のカスタマイズ
+        fig.update_layout(
+            width=1200,
+            height=200,
+            template="seaborn",
+            xaxis=dict(
+                tickvals=month_start_weeks,  # ラベルを表示するxの値（週番号）
+                ticktext=month_labels,  # 表示する月の名前
+                tickmode='array',  # ラベルを手動設定
+                scaleanchor="y",   # y軸に対してx軸をスケール固定
+                showgrid=False,    # グリッド線を非表示
+                constrain="domain"  # レイアウト全体に対してx軸のサイズを固定
+            ),
+            yaxis=dict(
+                showgrid=False,    # グリッド線を非表示
+                scaleanchor="x",   # x軸に対してy軸をスケール固定
+                tickvals=[0, 1, 2, 3, 4, 5, 6],
+                ticktext=['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],  # x軸ラベル
+                tickmode='array',  # ラベルを手動設定
+                constrain="domain"  # レイアウト全体に対してx軸のサイズを固定
+            ),
+            margin=dict(l=20, r=0, t=0, b=20),
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+
+if __name__ == "__main__":
+    st.markdown("# TrainingLogVisualizer")
+    obj = TrainingLogVisualizer()
+    st.markdown("## Contribution Map")
+    obj.plot_contribution_map()
+    st.markdown("## Recent Volume")
+    obj.visualize_volume()
